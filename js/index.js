394b4c1f1113f0b2566fc5a164f05855c81173ca
Viacheslav Chesnokov <83201823+snkio@users.noreply.github.com>
2026-02-20 18:12:13 +0500

    add heal mechanic and reward system

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const mainText = document.querySelector('.h1__text')
 const hpStatus = document.querySelector('#hp-status')
+const hpPotions = document.querySelector('#potions')
+const potionsMount = document.querySelector('#potionsMount')
 const gameStatus = document.querySelector('#status')
 const gameScore = document.querySelector('#score')
 const gameEnd = document.querySelector('#end')
@@ -8,10 +10,36 @@ const playBtn = document.querySelector('#playGame')
 const PLAYER = {
 	hp: 100,
 	score: 0,
+	hppotions: 5
 }
 
 gameRestart.addEventListener('click', () => {
 	location.reload()
+})
+
+hpPotions.addEventListener('click', () => {
+	if (PLAYER.hppotions <= 0) {
+		PLAYER.hppotions = 0
+		gameStatus.textContent = `У вас не хватает зелий восстановления`
+		return
+	}
+
+	if (PLAYER.hp >= 75) {
+		gameStatus.textContent = 'Поберегите зелья!'
+		return
+	}
+
+	PLAYER.hp += 25
+	PLAYER.hppotions--
+
+	if (PLAYER.hp > 100) {
+		PLAYER.hp = 100
+	}
+
+	gameStatus.textContent = `Вы выпили зелье! Осталось еще: ${PLAYER.hppotions}`
+
+	hpStatus.textContent = PLAYER.hp
+	potionsMount.textContent = `Зелий здоровья: ${PLAYER.hppotions}`
 })
 
 playBtn.addEventListener('click', () => {
@@ -24,6 +52,8 @@ playBtn.addEventListener('click', () => {
 	mainText.style.display = 'block'
 	gameScore.style.display = 'block'
 	gameStatus.style.display = 'block'
+	hpPotions.style.display = 'block'
+	potionsMount.style.display = 'block'
 
 
 	if (CHANCE < 0.1) {
@@ -39,14 +69,25 @@ playBtn.addEventListener('click', () => {
 		PLAYER.score += 15
 	}
 
+
 	gameScore.textContent = `Ваш счёт: ${PLAYER.score}`
 	hpStatus.textContent = PLAYER.hp
+
+	if (PLAYER.score >= 500 && !PLAYER.reward500) {
+		PLAYER.hppotions++
+		PLAYER.reward500 = true
+		gameStatus.textContent = 'Награда за 500 очков: 1 зелье!'
+	}
+
+	potionsMount.textContent = `Зелий здоровья: ${PLAYER.hppotions}`
 
 	if (PLAYER.hp <= 15) {
 		gameStatus.textContent = 'Генерал решил вас перевести в госпиталь вы были слишком ранены'
 		mainText.style.display = 'none'
 		gameScore.style.display = 'none'
 		playBtn.style.display = 'none'
+		hpPotions.style.display = 'none'
+		potionsMount.style.display = 'none'
 
 		gameRestart.style.display = 'block'
 
